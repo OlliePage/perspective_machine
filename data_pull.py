cbb07ab259408c30ac66e6e26f4f6f529056037a
olliepage <products15816@gmail.com>
2020-05-11 09:45:13 +0100

    created chart of UK population by age

--- data_pull.py
+++ data_pull.py
@@ -1,10 +1,13 @@
-#%%
+# %%
 
 from typing import List, Any, Dict
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
-#%%
+# %%
 from pandas import Series
 
 r = requests.get('https://api.beta.ons.gov.uk/v1/datasets')
@@ -17,10 +20,11 @@ for i in range(len(r.json()['items'])):
     dataset_ids.append(r.json()['items'][i]['id'])
 
 table_of_codes = pd.DataFrame(data={'title': dataset_titles,
-                   'description': dataset_descriptions,
-                  'dataset_id': dataset_ids})
+                                    'description': dataset_descriptions,
+                                    'dataset_id': dataset_ids})
 
-#%%
+
+# %%
 
 ## find information about a dataset
 # we need to know the following items to make a correct api call:
@@ -31,7 +35,7 @@ table_of_codes = pd.DataFrame(data={'title': dataset_titles,
 # * geography
 # * dimension
 
-#%% md
+# %% md
 #
 # > This allows querying of a single observation/value by providing one option per dimension, but will also allow one of
 # > these dimensions to be a ‘wildcard’ and return all values for this dimension.
@@ -45,7 +49,7 @@ table_of_codes = pd.DataFrame(data={'title': dataset_titles,
 # dimension3={dimension3ID}&
 # dimension4={dimension4ID}...`
 
-#%%
+# %%
 
 def post(extension, is_ok=False):
     r = requests.get(f'https://api.beta.ons.gov.uk/v1/' + extension)
@@ -53,7 +57,8 @@ def post(extension, is_ok=False):
         print(r)
     return r.json()
 
-#%% md
+
+# %% md
 
 # Create a chart from Population Estimates for UK, Wales, etc.
 #
@@ -67,50 +72,50 @@ def post(extension, is_ok=False):
 #
 # code: mid-year-pop-est
 
-#%%
+# %%
 
 dataset_id = 'mid-year-pop-est'
 uk_population_dataset = post(f'/datasets/{dataset_id}', is_ok=True)
 list(uk_population_dataset.keys())
 
-#%%
+# %%
 
 uk_population_dataset
 
-#%%
+# %%
 
 uk_population_dataset.keys()
 
-#%% md
+# %% md
 
 ## fetch the latest version of this dataset
 
-#%%
+# %%
 
 uk_pop_latest_version = uk_population_dataset['links']['latest_version']['id']
 uk_pop_latest_version
 
+# %%
 
-#%%
-
-uk_population_editions = post(f'/datasets/{dataset_id}/editions/time-series/versions/{uk_pop_latest_version}', is_ok=True)
+uk_population_editions = post(f'/datasets/{dataset_id}/editions/time-series/versions/{uk_pop_latest_version}',
+                              is_ok=True)
 uk_population_editions
 
-
-#%% md
+# %% md
 
 ##### fetch the dimensions that this dataset has to build the rest of the api.
 ##### We use the code list for this, a separate service
 
-#%%
+# %%
 
 uk_population_code_list = post(f'/code-lists/{dataset_id}', is_ok=True)
 
-#%%
+# %%
 
-dimensions = post(f'/datasets/{dataset_id}/editions/time-series/versions/{uk_pop_latest_version}/dimensions', is_ok=True)
+dimensions = post(f'/datasets/{dataset_id}/editions/time-series/versions/{uk_pop_latest_version}/dimensions',
+                  is_ok=True)
 
-#%%
+# %%
 
 dimensions['items'][3]
 # 'time'
@@ -118,14 +123,16 @@ dimensions['items'][3]
 # 'geography'
 # 'age'
 
-#%%
+# %%
 
 # check options for each dimension
 dimension = 'sex'
-options: Dict = post(f'/datasets/{dataset_id}/editions/time-series/versions/{uk_pop_latest_version}/dimensions/{dimension}/options', is_ok=True)
+options: Dict = post(
+    f'/datasets/{dataset_id}/editions/time-series/versions/{uk_pop_latest_version}/dimensions/{dimension}/options',
+    is_ok=True)
 options['items']
 
-#%%
+# %%
 
 # age = type in int for age, up to '90+'. Use wildcard if possible. e.g '21'
 # time = id is the year e.g '2010'
@@ -142,31 +149,52 @@ post(f'/datasets/{dataset_id}'
      f'sex=0&'
      f'age=*', is_ok=True)
 
-#%%
+# %%
 
 # extract the individual values
 
 observations: Dict = post(f'/datasets/{dataset_id}'
-     f'/editions/time-series'
-     f'/versions/4/'
-     f'observations?'
-     f'time=2017&'
-     f'geography=K02000001&'
-     f'sex=0&'
-     f'age=*', is_ok=True)
+                          f'/editions/time-series'
+                          f'/versions/4/'
+                          f'observations?'
+                          f'time=2017&'
+                          f'geography=K02000001&'
+                          f'sex=0&'
+                          f'age=*', is_ok=True)
 
-#%%
+# %%
 
 list(observations.keys())
 
-
-#%%
+# %%
 
 len(observations['observations'])
 
-#%%
+# %%
 
 observations_values = pd.DataFrame.from_dict(observations['observations'])['observation']
-#%%
-observations['observations'][0]['dimensions']
+# %%
 
+data = []
+for i in range(len(observations['observations'])):
+    try:
+        data.append((int(observations['observations'][i]['dimensions']['age']['label']),
+                     int(observations['observations'][i]['observation'])))
+    except ValueError:
+        data.append((observations['observations'][i]['dimensions']['age']['label'],
+                     observations['observations'][i]['observation']))
+    # %%
+# the below list comprehension removes these two string formatted arrays
+#  ('Total', '66040229'),
+#  ('90+', '579776'),
+data_ints = [item for item in data if type(item[0]) == int]
+remaining = [item for item in data if type(item[0]) == str]
+
+data_sorted = sorted(data_ints, key=lambda x: x[0])
+
+x, y = zip(*data_sorted)
+
+# %%
+fig, ax = plt.subplots()
+ax.bar(x, height=y)
+ax.set_title('Population Estimates for UK, Wales, etc by age, 2017')
